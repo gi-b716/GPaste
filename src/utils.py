46dd6f55dfe5c0c39f5d273ca53d147861943ecd
sganyunchi@163.com <78394473+gi-b716@users.noreply.github.com>
2025-03-04 22:27:10 +0800

    Upd

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
 from app import app, db, User
+import logging, time
+
+logger = logging.getLogger('logger')
+logger.setLevel(logging.DEBUG)
+logFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logFile = logging.FileHandler("log/utils.log", encoding="utf-8", mode="a")
+logFile.setLevel(logging.DEBUG)
+logFile.setFormatter(logFormatter)
+logger.addHandler(logFile)
 
 def get_admin(args):
     user_id = args.user_id
@@ -7,11 +16,13 @@ def get_admin(args):
         user.is_admin = True
         db.session.commit()
         print("User {} is now an admin".format(user.username))
+        logger.info("User {} is now an admin".format(user.username))
 
 def init_db(args):
     with app.app_context():
         db.create_all()
         print("Database initialized")
+        logger.info("Database initialized")
 
 def allow_all_users(args):
     with app.app_context():
@@ -21,6 +32,7 @@ def allow_all_users(args):
             user.allow_login = True
         db.session.commit()
         print("All users are now allowed to create and login")
+        logger.info("All users are now allowed to create and login")
 
 import argparse
 
